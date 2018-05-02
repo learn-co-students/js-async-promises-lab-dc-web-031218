d8bbf147873aba9e412cc080bd72e25a303c1fe7
Austin Wolfe <wolfe_austin@yahoo.com>
2018-05-02 14:56:38 -0400

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,66 @@ const questions = [
 ]
 
 let question;
+
+function appendQuestion(question){
+  // const cont = document.getElementsByClassName('question-container')[0]
+  // container.innerHTML = question.questionText;
+  const cont = document.querySelector('.question-container')
+  cont.innerHTML = question.questionText
+  // cont.appendChild(question.questionText)
+  // debugger;
+  // cont.getElementsByTagName('div')
+  // let li = document.createElement('li')
+  // li.innerHTML = question.toString();
+  // cont.appendChild(li);
+}
+
+function askQuestionThen(time){
+  // question = questions[0];
+  // appendQuestion(question)
+  // return new Promise((resolve, reject) => {
+  //   setTimeout(function(){
+  //     resolve()
+  //   },time);
+  // })
+  question = questions[0]
+  appendQuestion(question)
+  return new Promise(function(resolve){
+    setTimeout(function(){
+      resolve(question)
+    }, time)
+  })
+}
+
+function removeQuestion(){
+  const cont = document.querySelector('.question-container')
+  cont.innerHTML = '';
+}
+
+function askQuestionThenRemoveQuestion(time){
+  // //ask question
+  // const cont = document.querySelector('.question-container')
+  // cont.innerHTML = questions[0].questionText
+  // // debugger;
+  // askQuestionThen(time).then(t =>removeQuestion())
+  return askQuestionThen(time).then(removeQuestion)
+
+}
+
+function trueAndFalseButtons(){
+  return btns = document.querySelector('.true-false-list').querySelectorAll('.btn')
+}
+
+function toggleTrueAndFalseButtons(){
+  trueAndFalseButtons().forEach(function(btn){
+    btn.classList.toggle("hide")
+  })
+}
+
+function displayQuestionOnClick(){
+  let btn = document.querySelector('a')
+  return btn.addEventListener('click', () => {
+    toggleTrueAndFalseButtons()
+    askQuestionThenRemoveQuestion(5000)
+  })
+}
